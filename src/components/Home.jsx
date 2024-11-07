c8de94e26c7cba13b656e2aeebdd5b6bdd87811d
AdamDev <a.diaz.1987.08@hotmail.com>
2024-11-07 17:02:05 -0300

    finalizando trabajo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,27 +5,25 @@ import { useState, useEffect } from "react";
 // import { pizzas } from "../Pizza";
 
 function Home() {
-  const [listpizzas, setListPizzas] = useState([])
+  const [listpizzas, setListPizzas] = useState([]);
 
   // funcion llamar API
 
   const callAPI = async () => {
     try {
-     const url = "http://localhost:5000/api/pizzas";
-     const response = await fetch(url);
-     const data = await response.json()
-     setListPizzas(data)
+      const url = "http://localhost:5000/api/pizzas";
+      const response = await fetch(url);
+      const data = await response.json();
+      setListPizzas(data);
+    } 
+    catch(e)  {
+      alert(e.message);
     }
-catch{
-  
-   alert("ocurrio un error")
-}
-  }
+  };
 
   useEffect(() => {
-    callAPI()
-  }, [])
-
+    callAPI();
+  }, []);
 
   return (
     <>
@@ -34,18 +32,19 @@ catch{
           <Header
             title="¡Pizzería Mamma Mía!"
             description="Tenemos las mejores pizzas que podras encontar."
-          /> 
+          />
         </div>
       </div>
-      <div className="contenedorCards d-flex flex-wrap gap-4 justify-content-center mt-5" >
-        {listpizzas.map( (pizza) =>
-        <CardsPizza key={pizza.id}
-          name = {pizza.name}
-          price={pizza.price}
-          ingredients = {pizza.ingredients}
-          img= {pizza.img}
-        /> )}
-
+      <div className="contenedorCards d-flex flex-wrap gap-4 justify-content-center mt-5">
+        {listpizzas.map((pizza) => (
+          <CardsPizza
+            key={pizza.id}
+            name={pizza.name}
+            price={pizza.price}
+            ingredients={pizza.ingredients}
+            img={pizza.img}
+          />
+        ))}
       </div>
     </>
   );
